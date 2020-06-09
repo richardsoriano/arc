5a3fce099769839b0145dafdf6665b7156801032
richardsoriano <soriano.richard2020@gmail.com>
2020-06-09 15:58:08 -0700

    fixed title client

---
+++ public/build/precache-manifest.0fa873bbfebac170431f35467c04cd4a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f27a39ab29ba700f5d15ae94e00acfcf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c4bcbd31d2833c80526",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "8ce4f37209420d051ab6",
+    "url": "/static/css/main.967b14ab.chunk.css"
+  },
+  {
+    "revision": "2c4bcbd31d2833c80526",
+    "url": "/static/js/2.d318d2d1.chunk.js"
+  },
+  {
+    "revision": "572bfeb62474f0cec44e259ca5a99f18",
+    "url": "/static/js/2.d318d2d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ce4f37209420d051ab6",
+    "url": "/static/js/main.4aa18f09.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
